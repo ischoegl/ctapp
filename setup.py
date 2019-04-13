2a8220357d58d14d87a116944d4cb542c768cb43
Ingmar Schoegl <ischoegl@lsu.edu>
2019-04-12 20:57:45 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
 import pkg_resources
 import glob
@@ -8,6 +8,11 @@ __CANTERA_OBJ = pkg_resources.resource_filename('cantera', '_cantera.*so')
 __CANTERA_OBJ = glob.glob(__CANTERA_OBJ)[0]
 
 __CANTERA_DEP = pkg_resources.resource_filename('cantera', 'interrupts.py')
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
 
 extensions = [
     Extension(
@@ -21,15 +26,21 @@ extensions = [
 
 setup(
     name="ctapp",
-    description=
-    'Example for compilation of custom classes against stock cantera',
+    description='Example for compilation of custom classes against stock cantera',
+    long_description=readme(),
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'License :: MIT',
         'Programming Language :: Python :: 3',
     ],
+    keywords='cantera',
+    url='https://github.com/ischoegl/ctapp.git',
+    author='Ingmar Schoegl',
+    author_email='ischoegl@lsu.edu',
+    license='MIT',
     install_requires=['cantera'],
     packages=find_packages(),
+    install_requires=['cantera>=2.4.0'],
     ext_modules=cythonize(extensions))
