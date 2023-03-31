7ab557b74f26481e9470af77033f05a23ec3e143
Ingmar Schoegl <ischoegl@lsu.edu>
2023-03-31 11:47:02 -0500

    Update sample path

--- examples/get_examples.sh
+++ examples/get_examples.sh
@@ -1,5 +1,5 @@
 # get two examples
-wget https://raw.githubusercontent.com/Cantera/cantera/master/interfaces/cython/cantera/examples/onedim/burner_flame.py
+wget https://raw.githubusercontent.com/Cantera/cantera/main/samples/python/onedim/burner_flame.py
 
 mv burner_flame.py cloned_flame.py
 sed -i 's/burner_flame/cloned_flame/g' cloned_flame.py
@@ -11,9 +11,8 @@ sed -i 's/cloned_flame/new_flame/g' new_flame.py
 sed -i 's/ctapp.ClonedFlame/ctapp.NewFlame/g' new_flame.py
 sed -i 's/width=width/width=width, nextra=5/g' new_flame.py
 
-wget https://raw.githubusercontent.com/Cantera/cantera/master/interfaces/cython/cantera/examples/reactors/reactor1.py
+wget https://raw.githubusercontent.com/Cantera/cantera/main/samples/python/reactors/reactor1.py
 
 mv reactor1.py cloned_reactor.py
 sed -i 's/ct.IdealGasConstPressureReactor/ctapp.ClonedReactor/g' cloned_reactor.py
 sed -i 's/import cantera as ct/import cantera as ct\nimport ctapp/g' cloned_reactor.py
-
