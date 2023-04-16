1677103b12ec48d4a433aed6eccab8399db654bb
Ingmar Schoegl <ischoegl@lsu.edu>
2023-04-16 09:33:28 -0500

    Add ctapp to samples

--- examples/get_examples.sh
+++ examples/get_examples.sh
@@ -5,9 +5,11 @@ mv burner_flame.py new_flame.py
 sed -i 's/burner_flame/new_flame/g' new_flame.py
 sed -i 's/ct.BurnerFlame/ctapp.NewFlame/g' new_flame.py
 sed -i 's/import cantera as ct/import cantera as ct\nimport ctapp/g' new_flame.py
+sed -i 's/cantera >= 3.0/cantera >= 3.0, ctapp/g' new_flame.py
 
 wget https://raw.githubusercontent.com/Cantera/cantera/main/samples/python/reactors/reactor1.py
 
 mv reactor1.py cloned_reactor.py
 sed -i 's/ct.IdealGasConstPressureReactor/ctapp.ClonedReactor/g' cloned_reactor.py
 sed -i 's/import cantera as ct/import cantera as ct\nimport ctapp/g' cloned_reactor.py
+sed -i 's/cantera >= 2.5.0, /cantera >= 3.0, ctapp, /g' cloned_reactor.py
