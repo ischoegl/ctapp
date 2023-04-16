4f7c08f831821574eb7373b1293a63c4c4570537
Ingmar Schoegl <ischoegl@lsu.edu>
2023-04-16 09:19:45 -0500

    Version 0.1.0

--- ctapp/__init__.py
+++ ctapp/__init__.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
-
 from ._ctapp import *
 from .clonedflame import *
+from ._version import *
---
+++ ctapp/_version.py
@@ -0,0 +1,5 @@
+__author__ = "Ingmar Schoegl"
+__version_info__ = (0, 1, 0)
+__version__ = '.'.join(map(str, __version_info__[:3]))
+if len(__version_info__) == 4:
+    __version__ += __version_info__[-1]
